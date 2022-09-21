9cac86960697e611f17d48fddacf41d5007497e8
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-09-21 11:54:17 -0400

    live

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/admin'
+    // 'mongoUrl' : 'mongodb://localhost:27017/admin'
+    'mongoUrl' : 'mongodb+srv://admin:1234@cluster0.u7cpej2.mongodb.net/admin-dashboard?retryWrites=true&w=majority'
 }
